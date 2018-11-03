d695b2899d79c61247e9bc0477ae2c4e5ca698d0
DESKTOP-LE084F3\FlorianB <florian.bonniec22@gmail.com>
2018-11-03 18:22:04 +0100

    API over HTTPS

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_ROOT = 'http://apiblog.florianbonniec.com/api/v1';
+const API_ROOT = 'https://apiblog.florianbonniec.com/api/v1';
 
 //endpoint /post
 export function loadArticles(){
